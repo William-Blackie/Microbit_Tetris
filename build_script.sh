99ab8d7ed592fec790becb4fba84fc7b2dd3b184
William Blackie <william2.blackie@live.uwe.ac.uk>
2018-01-29 20:42:50 +0000

    blocks sit on ground and if bottom row is full the row is deleted, added bash script for easy compiling/running

---
+++ build_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+yt build
+cp ./build/bbc-microbit-classic-gcc/source/iot-example-combined.hex /media/student/MICROBIT
--- source/main.cpp
+++ source/main.cpp
@@ -11,19 +11,53 @@ MicroBit uBit;
 MicroBitImage LBlock("255,0\n255,0\n255,255\n"); //L shaped block
 MicroBitImage SqrBlock("255,255\n255,255\n"); // Square shaped block
 MicroBitImage StrBlock("255,255,255,0\n"); // straight shaped block
-MicroBitImage Stage("0,0,0,0,0\n0,0,0,0,0\n0,0,0,0,0\n0,0,0,0,0\n0,0,0,0,0\n");
-
+MicroBitImage Stage("0, 0, 0, 0, 0\n 0, 0, 0, 0, 0\n 0, 0, 0, 0, 0\n 0, 0, 0, 0, 0\n 0, 0, 0, 0, 0\n ");
+MicroBitImage PlayerSpace(Stage);
+bool MoveLeft, MoveRight = false;
+//int X, Y = 0;
 
 void onButtonA(MicroBitEvent e)
 {
-    Stage.shiftLeft(1);
+  MoveLeft = true;
     //move left
 }
 
 void onButtonB(MicroBitEvent e)
 {
-    Stage.shiftRight(1);
-    //move right
+  MoveRight = true;
+  //move right
+}
+
+void moveBlock(int X, int Y){
+  if(Y <= 3){ //prevents blocks going offscreen
+    PlayerSpace.setPixelValue(X,Y,255);//set new pixel
+    PlayerSpace.setPixelValue(X,Y-1,0); //whipe old pixel
+
+    PlayerSpace.setPixelValue(X+1,Y,255);//set new pixel
+    PlayerSpace.setPixelValue(X+1,Y-1,0); //whipe old pixel
+
+    PlayerSpace.setPixelValue(X,Y+1,255);//set new pixel
+    PlayerSpace.setPixelValue(X+1,Y-1,0); //whipe old pixel
+
+    PlayerSpace.setPixelValue(X+1,Y+1,255);//set new pixel
+    PlayerSpace.setPixelValue(X-1,Y-1,0); //whipe old pixel
+  }
+}
+
+bool checkBottomLine(int X, int Y){
+  int counter = 0;
+
+  for (int j =0; j <= 4; j++){
+    counter +=  PlayerSpace.getPixelValue(j,4);
+  }
+  if(counter >= 1275){
+    moveBlock(X, Y+1);
+    PlayerSpace.shiftDown(1); //remove bottom row if full
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 int main()
@@ -32,26 +66,46 @@ int main()
     uBit.init();
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
     uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
+
     //add gyro to tell user to hold micro bit right way up
-    //shaking game increase brightness?
+    //shaking game to clear ground blocks
     // Insert your code here
-         //paste(img,left most x cords, (highest y cord)
 
     /*
-        0 1 2 3 4
+        0 1 2 3 4 X
     0   x x x x x
     1   x x x x x
     2   x x x x x
     3   x x x x x
     4   x x x x x
+    Y
     */
-      Stage.paste(StrBlock,1,0);
-    for(int i = 0; i <=4; i++){
-        uBit.display.print(Stage);
-        uBit.sleep(750);
-        Stage.shiftDown(1);
-      }
 
+
+while(true){
+  int X = 1;
+  bool lineCheck = false;
+for(int Y = 0; Y <= 4; Y++){
+    uBit.sleep(750);
+    uBit.display.print(PlayerSpace);
+    if(MoveLeft == true){
+      X--;
+      moveBlock(X,Y);
+      MoveLeft = false;
+    }
+    else if(MoveRight == true){
+      X++;
+      moveBlock(X,Y);
+      MoveRight = false;
+    }
+    else{
+      moveBlock(X,Y); //move down
+      if(checkBottomLine(X, Y) == true){
+        Y++;
+      }
+    }
+    }
+  }
     uBit.display.clear(); //whipe display
 
 
